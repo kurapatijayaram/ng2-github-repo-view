b813c7a8e1133e2d1df0dc93be511683e164260a
Jayaram K <kurapatijayaram@gmail.com>
2016-09-23 02:20:13 +0530

    Phase - I. Displaying only master branch.

--- example/home.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: 'home',
-  template: `<div class="container"><h1>Angular2 Demo</h1><router-outlet></router-outlet></div>`
-})
-export class HomeComponent {
-
-}
--- src/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./repo-viewer.components";
-export * from "./repo-viewer.service";
