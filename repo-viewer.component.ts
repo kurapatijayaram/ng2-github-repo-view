5af684ce341d6054646d544c658c7d4255a17212
Jayaram K <kurapatijayaram@gmail.com>
2016-09-25 02:21:15 +0530

    breadcumbs.

--- repo-viewer.component.ts
+++ repo-viewer.component.ts
@@ -7,15 +7,20 @@ import { RepoViewerService } from "./repo-viewer.service";
   template: `
               <div class="row">
                 <div class="col-md-12">
-                  <div class="list-group" *ngIf="_rvs.currentData?.tree">
-                    <a style="cursor: pointer;" (click)="_rvs.fetchData(item.type, item.sha)" class="list-group-item"  *ngFor="let item of _rvs.currentData.tree">
+                  <ol class="breadcrumb">
+                    <li *ngFor="let crumb of _rvs.breadcrumbs;let lastIndex = last"  [ngClass]="{'active': lastIndex}">
+                      <a (click)="breadcrumbClick(crumb, lastIndex)">{{crumb.path}}</a>
+                    </li>
+                  </ol>
+                  <div class="list-group" *ngIf="_rvs.currentData?.type=='tree'">
+                    <a style="cursor: pointer;" (click)="_rvs.fetchData(item)" class="list-group-item"  *ngFor="let item of _rvs.currentData.treeObjects">
                       <span class="glyphicon" [ngClass]="{'glyphicon-file': (item.type=='blob'), 'glyphicon-folder-close': (item.type=='tree')}">
                         {{item.path}}
                       </span>
                     </a>
                   </div>
-                  <textarea *ngIf="_rvs.currentData?.content" class="form-control" rows="15" cols="35">
-                    {{_window.atob(_rvs.currentData.content)}}
+                  <textarea *ngIf="_rvs.currentData?.type=='blob'" class="form-control" rows="15" cols="35">
+                    {{_window.atob(_rvs.currentData.treeObjects.content)}}
                   </textarea>
                 </div>
               </div>
@@ -23,14 +28,16 @@ import { RepoViewerService } from "./repo-viewer.service";
   styleUrls: ['css/bootstrap.min.css'],
   providers: [RepoViewerService, {provide: Window, useValue: window}]
 })
-export class RepoViewer implements OnInit{
+export class RepoViewer{
   @Input("handle")
   set handle(value: string){
     this._rvs.setUrl(value);
   }
   constructor(private _rvs: RepoViewerService, private _window: Window){}
 
-  ngOnInit(){
-
+  breadcrumbClick(treeObj, last){
+    if(!last){
+      this._rvs.fetchData(treeObj)
+    }
   }
 }
--- repo-viewer.service.ts
+++ repo-viewer.service.ts
@@ -1,13 +1,21 @@
 import { Injectable } from "@angular/core";
 import { RESTClient, BaseUrl, GET, Path } from "angular2-rest";
-import { Observable } from "rxjs/Rx";
+import 'rxjs/Rx';
 import { Http } from "@angular/http";
+
+export interface ITreeObject{
+  type: string,
+  sha: string,
+  path: string
+}
 
 @Injectable()
 @BaseUrl("https://api.github.com")
 export class RepoViewerService extends RESTClient{
   repoUrl: string;
   currentData: any;
+  repoName: string;
+  breadcrumbs: ITreeObject[]=[];
   private _userRepoPath: string;
 
   constructor(private _http: Http){
@@ -15,37 +23,40 @@ export class RepoViewerService extends RESTClient{
   }
 
   setUrl(url: string){
-    this.repoUrl = url
-    this._userRepoPath = this.repoUrl.replace("https://github.com/", "")
+    this.repoUrl = url;
+    this._userRepoPath = this.repoUrl.replace("https://github.com/", "");
+    this.repoName = this._userRepoPath.split("/")[1];
     this._fetchMaster(this._userRepoPath).subscribe(
       (data) => {
-                  this.fetchData("tree", data.json().commit.commit.tree.sha)
+                  this.fetchData({type: "tree", sha: data.json().commit.commit.tree.sha, path: this.repoName})
                 },
       (error) => {console.log(error)}
-    )
+    );
   }
 
-  fetchData(type: string, shaId: string){
-    switch(type){
-      case "tree":
-                  type="trees";
-                  break;
-      case "blob":
-                  type="blobs";
-                  break;
+  fetchData(treeObj: ITreeObject){
+    let bIndex = this.breadcrumbs.indexOf(treeObj);
+    if(bIndex == -1){
+      this.breadcrumbs.push(treeObj);
+    }else{
+      this.breadcrumbs.splice(bIndex+1, this.breadcrumbs.length - (bIndex+1));
     }
-
     this._fetchTree(
           this._userRepoPath,
-          type,
-          shaId
-        ).subscribe(
-          (tree) => {this.currentData = tree.json()},
+          treeObj.type,
+          treeObj.sha
+        ).map(function(val, idx, obs){
+          return {
+                    "type": treeObj.type,
+                    "treeObjects": (treeObj.type == "tree") ? JSON.parse(val._body).tree : JSON.parse(val._body)
+                  }
+        }).subscribe(
+          (data) => {this.currentData = data},
           (error) => {console.log(error)}
-        )
+        );
   }
 
-  @GET("/repos/{userRepo}/git/{type}/{shaId}")
+  @GET("/repos/{userRepo}/git/{type}s/{shaId}")
   private _fetchTree(@Path("userRepo") url: string, @Path("type") type: string, @Path("shaId") shaId: string){
     return null;
   }
